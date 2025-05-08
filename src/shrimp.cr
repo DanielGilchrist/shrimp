f4f354662bc603e884d6f28bacaaf033a3964dee
Daniel Gilchrist <danniieelg@gmail.com>
2025-05-08 22:13:56 +0100

    Add https://github.com/Timendus/chip8-test-suite as a submodule for testing

--- src/shrimp.cr
+++ src/shrimp.cr
@@ -1,3 +1,4 @@
+require "option_parser"
 require "./shrimp/**"
 
 module Shrimp
@@ -6,13 +7,52 @@ module Shrimp
   VERSION = "0.1.0"
 
   def main
+    options = parse_options!
+
     interpreter = Interpreter.new
-    interpreter.load_rom(String.new)
+    interpreter.load_rom(options.rom_path)
 
     loop do
       interpreter.cycle
     end
   end
+
+  private def parse_options! : Options
+    rom_path = ""
+
+    OptionParser.parse do |parser|
+      parser.banner = "Usage: shrimp [arguments]"
+      parser.on("-r PATH", "--rom=PATH", "Specifies the path to a ROM") { |path| rom_path = path }
+      parser.on("-h", "--help", "Show this help") do
+        puts parser
+        exit
+      end
+
+      parser.invalid_option do |flag|
+        error!("ERROR: #{flag} is not a valid option.") do
+          STDERR.puts parser
+        end
+      end
+    end
+
+    if rom_path.blank?
+      error!("--rom must be provided!")
+    end
+
+    Options.new(rom_path)
+  end
+
+  private def error!(message : String)
+    error!(message) {}
+  end
+
+  private def error!(message : String, &)
+    STDERR.puts(message)
+    yield
+    exit(1)
+  end
+
+  private record Options, rom_path : String
 end
 
 Shrimp.main
